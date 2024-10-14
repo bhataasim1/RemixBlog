8788e8b5b6ec1be6eaa53dc8d87a095043756d8b
bhataasim1 <bhataasim700@gmail.com>
2024-10-14 13:39:33 +0530

    Add toggle-mode component to add dark mode functionality

---
+++ app/components/toggle-theme/toggle-mode.tsx
@@ -0,0 +1,21 @@
+import { ActionIcon, useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
+import { IconSun, IconMoon } from '@tabler/icons-react';
+import cx from 'clsx';
+import classes from './toggle-mode.module.css';
+
+export function ToggleMode() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+
+  return (
+    <ActionIcon
+      onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+      variant="default"
+      size="lg"
+      aria-label="Toggle color scheme"
+    >
+      <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+      <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+    </ActionIcon>
+  );
+}
