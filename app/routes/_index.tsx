0f50e086994c887f83d6fff127bb218b0fdb8365
bhataasim1 <bhataasim700@gmail.com>
2024-10-14 18:16:18 +0530

    Refactor PostCard component to use dynamic data from props and fetch posts from server

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,10 @@
 import { Box } from "@mantine/core";
 import type { MetaFunction } from "@remix-run/node";
 import { PostCard } from "../components/post-card/post-card";
+import { PostServices } from "../.server/blog/PostService.server";
+import { useLoaderData } from "@remix-run/react";
+
+const postServices = new PostServices();
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,10 +13,16 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  return await postServices.getAllPosts();
+}
+
 export default function Index() {
+  const posts = useLoaderData<typeof loader>();
+  console.log("Loader Data", posts);
   return (
     <Box size={'xl'}>
-      <PostCard />
+      <PostCard posts={posts} />
     </Box>
   );
 }
